e573a293b120b0d961de4352ef902f1a05ac6ed3
Renato Gonçalves <renatoslip@hotmail.com>
2020-03-11 15:15:11 -0300

    driver e sessão

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Log;
-
+use Illuminate\Support\Facades\Session;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,4 +22,40 @@ Route::get('/', function () {
 Route::get('/log', function() {
     //Log::channel('slack')->info('teste');
     Log::stack(['slack', 'daily'])->info('teste');
+});
+
+Route::get('/session', function() {
+    session([
+        'course' => 'LaraDev'
+    ]);
+    session()->put('name', 'Gustavo Web');
+    
+    // echo session()->get('name');
+
+    // session()->put('time', []);
+
+    // session()->push('time', 'Gustavo Web');
+    
+    // $student = session()->pull('name');
+
+    // echo $student;
+    // session()->forget('name'); //apaga a key name mas sem retorno
+
+    // session()->flush(); //apaga o array de toda a session
+
+    // if(session()->has('course')){
+    //     echo "O curso selecionado foi: ". session()->get('course')."<br>";
+    // }
+
+    // if(session()->exists('course')){
+    //     echo "Esse indice existe";
+    // } else {
+    //     echo "Esse indice não existe";
+    // }
+    
+    session()->flash('message', 'O artigo foi publicado com sucesso');
+    
+    session()->reflash();
+    
+    dump(session()->all());
 });
